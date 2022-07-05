a835a9e7390a8085fc61310f82b9867e227f0ad4
Marwan Zayed <92050744+marwanzayed-coder@users.noreply.github.com>
2022-07-05 21:48:56 +0200

    Create bin_to_oct.js

---
+++ bin_to_oct.js
@@ -0,0 +1,8 @@
+function bin_to_oct(b) {
+  return parseInt(b, 2).toString(8);
+}
+
+console.log(oct_to_bin("101010101010"));
+console.log(oct_to_bin("1000000000"));
+console.log(oct_to_bin("10101111000"));
+console.log(oct_to_bin("1"));
