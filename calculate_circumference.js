c9b76db275f142db95d28669f92a1e932219a02d
Marwan Zayed <92050744+marwanzayed-coder@users.noreply.github.com>
2022-07-05 02:37:31 +0200

    Create calculate_circumference.js

---
+++ calculate_circumference.js
@@ -0,0 +1,8 @@
+function calculate_circumference(radius) {
+  return 2 * radius * Math.PI;
+}
+
+console.log(calculate_circumference(2));
+console.log(calculate_circumference(3.1));
+console.log(calculate_circumference(4.9));
+console.log(calculate_circumference(5));
