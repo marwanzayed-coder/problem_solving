d37f8fe575f877596e9e7aa6632adb0dc2390042
Marwan Zayed <92050744+marwanzayed-coder@users.noreply.github.com>
2022-07-05 02:06:49 +0200

    Create find_circle_area.js

---
+++ find_circle_area.js
@@ -0,0 +1,8 @@
+function find_circle_area(radius) {
+  return 3.14 * Math.pow(radius, 2);
+}
+
+console.log(find_circle_area(5.4));
+console.log(find_circle_area(7));
+console.log(find_circle_area(1));
+console.log(find_circle_area(10.2));
