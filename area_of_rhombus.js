3a13879f415eb4a7801b0f42802b0666d724c9eb
Marwan Zayed <92050744+marwanzayed-coder@users.noreply.github.com>
2022-07-05 20:41:23 +0200

    Create area_of_rhombus.js

---
+++ area_of_rhombus.js
@@ -0,0 +1,8 @@
+function area_of_rhombus(q, p) {
+  return q * p * 0.5;
+}
+
+console.log(area_of_rhombus(6, 7));
+console.log(area_of_rhombus(3, 6.6));
+console.log(area_of_rhombus(8.7, 9.1));
+console.log(area_of_rhombus(9.2, 8.6));
