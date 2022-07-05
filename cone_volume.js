9c345eec40b6c781d3b9f5067ce60abc7778ab9e
Marwan Zayed <92050744+marwanzayed-coder@users.noreply.github.com>
2022-07-05 02:18:47 +0200

    Create cone_volume.js

---
+++ cone_volume.js
@@ -0,0 +1,8 @@
+function cone_volume(radius, height) {
+  return (1 / 3) * 3.14 * Math.pow(radius, 2) * height;
+}
+
+console.log(cone_volume(1.3, 5.2));
+console.log(cone_volume(8.7, 7.8));
+console.log(cone_volume(3.5, 5.1));
+console.log(cone_volume(1, 4.2));
